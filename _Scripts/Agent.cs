6cbc9f00a28f153555b0b9aef291c1c2bf06b053
rLeblancTemp <rmi.remi@hotmail.fr>
2016-04-16 11:53:08 +0200

    Energy System

---
+++ _Scripts/Agent.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Agent : MonoBehaviour 
+{
+	// PROPERTIES //
+	public Vector2 Position2D
+	{
+		get
+		{
+			return new Vector2(transform.position.x, transform.position.y);
+		}
+		set
+		{
+			transform.position = new Vector3(value.x, value.y, transform.position.z);
+		}
+	}
+
+	private float _energy;
+	public float Energy
+	{
+		get
+		{
+			return _energy;
+		}
+		set
+		{
+			_energy = value;
+			if (_energy < 0)
+			{
+				//kill Agent
+			}
+		}
+	}
+
+	public float EnergyConsumptionPerSecond;
+	public float EnergyConsumedPerUnityUnit;
+}
---
+++ _Scripts/EnergySystem.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnergySystem : MonoBehaviour 
+{
+	// PROPERTIES //
+	private Agent _agent;
+	private Vector2 _agentLastPosition;
+	private bool _firstFramePassed = false;
+
+	// MONOBEHAVIOR //
+	void Awake()
+	{
+		try
+		{
+			_agent = GetComponent<Agent>();
+		}
+		catch
+		{
+			Debug.LogError("No Agent Found");
+		}
+	}
+
+	void Start () 
+	{
+		StartCoroutine(IdleConsumption(_agent.EnergyConsumptionPerSecond));
+	}
+
+	void Update () 
+	{
+		if (_firstFramePassed && _agent.Position2D != _agentLastPosition)
+		{
+			_agent.Energy -= Vector2.Distance(_agent.Position2D, _agentLastPosition);
+		}
+		_agentLastPosition = _agent.Position2D;
+		_firstFramePassed = true;
+	}
+
+	// METHODS //
+	private IEnumerator IdleConsumption(float energyConsumedPerSecond)
+	{
+		float t;
+		while (true)
+		{
+			t=1;
+			while(t>0)
+			{
+				t -= Time.deltaTime;
+				yield return new WaitForEndOfFrame();
+			}
+		}
+	}
+}
