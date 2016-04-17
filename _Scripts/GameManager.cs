67c737e501ef8632afd45e6110bd97e8bda8cd8d
prtesla <t.totota@gmail.com>
2016-04-17 13:18:39 +0200

    Scene and Manager

--- _Scripts/GameManager.cs
+++ _Scripts/GameManager.cs
@@ -1,20 +1,22 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour {
 
+    public Text sliderText;
+
 	// Use this for initialization
 	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+	    if(sliderText == null)
+        {
+            Debug.LogError("Slider Text missing in script !");
+        }
 	}
 
     public void ChangeSpeed(float speed)
     {
         Time.timeScale = speed;
+        sliderText.text = "Time Scale : x" + speed.ToString("F01");
     }
 }
